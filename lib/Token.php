77665b96655a607901ea1d4413c06cab987b4bfa
Mike Curtis <mikecurtis1@gmail.com>
2016-08-12 12:36:23 -0400

    Create Token.php

---
+++ lib/Token.php
@@ -0,0 +1,72 @@
+<?php 
+
+class Token
+{
+    private $prefix;
+    private $index;
+    private $text;
+    private $phrase;
+
+    public function __construct($prefix='', $index='', $text='', $phrase=false){
+        $this->prefix = $this->setPrefix($prefix);
+        $this->index = $this->setPrefix($index);
+        $this->text = $this->setPrefix($text);
+        $this->phrase = $this->setPrefix($phrase);
+    }
+    
+    public function setPrefix($str)
+    {
+        if (is_string($str)) {
+            $this->prefix = $str;
+        } else {
+            $this->prefix = '';
+        }
+    }
+    
+    public function setIndex($str)
+    {
+        if (is_string($str)) {
+            $this->index = $str;
+        } else {
+            $this->index = '';
+        }
+    }
+    
+    public function setText($str)
+    {
+        if (is_string($str)) {
+            $this->text = $str;
+        } else {
+            $this->text = '';
+        }
+    }
+    
+    public function setPhrase($bool)
+    {
+        if (is_bool($bool)) {
+            $this->phrase = $bool;
+        } else {
+            $this->phrase = false;
+        }
+    }
+    
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+    
+    public function getIndex()
+    {
+        return $this->index;
+    }
+    
+    public function getText()
+    {
+        return $this->text;
+    }
+    
+    public function getPhrase()
+    {
+        return $this->phrase;
+    }
+}
